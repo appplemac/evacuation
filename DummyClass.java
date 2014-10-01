0ff43798668f5562d5be2859d9f1cb55e79c5d97
maximino.puncernau <max-725@hotmail.com>
2014-10-01 19:11:03 +0200

    Add dummy class

---
+++ DummyClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maximino.puncernau
+ * Date: 1/10/14
+ * Time: 19:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class DummyClass {
+    void main() {
+    }
+}
