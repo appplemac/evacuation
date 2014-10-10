3e8eb6cf0e6241eabef33bc1c6bc9849fdf05384
adria.abella <adria.abella.93@gmail.com>
2014-10-10 14:47:31 +0200

    Pair with class templates

--- Pair.java
+++ Pair.java
@@ -7,24 +7,24 @@ package com.company;
  * Time: 14:09
  * To change this template use File | Settings | File Templates.
  */
-public class Pair {
-    private Object first;
-    private Object second;
+public class Pair<class1, class2> {
+    private class1 first;
+    private class2 second;
 
-    Pair(Object a, Object b){
+    Pair(class1 a, class2 b){
             first = a;
             second = b;
     }
 
-    public Object getFirst(){
+    public class1 getFirst(){
             return first;
     }
 
-    public Object getSecond(){
+    public class2 getSecond(){
         return second;
     }
 
-    public void setSecond(Object b){
+    public void setSecond(class2 b){
          second = b;
     }
 }
