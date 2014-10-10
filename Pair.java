18bd2abeea50ef0f046e66af66ab8a186ca212f8
maximino.puncernau <max-725@hotmail.com>
2014-10-10 14:28:50 +0200

    Swap function added, and getters for Group as well

---
+++ Pair.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maximino.puncernau
+ * Date: 10/10/14
+ * Time: 14:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class Pair {
+    private Object first;
+    private Object second;
+
+    Pair(Object a, Object b){
+            first = a;
+            second = b;
+    }
+
+    public Object getFirst(){
+            return first;
+    }
+
+    public Object getSecond(){
+        return second;
+    }
+}
