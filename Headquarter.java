ecaa94f98eaa6c9eb629a0864550f1eac0cc8576
Alexey <appplemac@gmail.com>
2014-10-16 16:41:33 +0200

    Removed the wrong thing

--- Headquarter.java
+++ Headquarter.java
@@ -11,19 +11,23 @@ import java.util.ArrayList;
  * @author Usuario
  */
 public class Headquarter {
+    private int HQcoordx;
+    private int HQcoordy;
     private int HQident;
     private ArrayList<Helicopter> helicopters;
-    
-    
-    public Headquarter(int id){
+
+
+    public Headquarter(int id,int x, int y){
         HQident = id;
+        HQcoordx= x;
+        HQcoordy = y;
         helicopters = new ArrayList<Helicopter>();
     }
-    
+
     public int getIdent(){
         return HQident;
     }
-    
+
     public ArrayList<Helicopter> getHelicopters(){
         return helicopters;
     }
@@ -35,8 +39,15 @@ public class Headquarter {
     public int getNumHelicopters() {
         return helicopters.size();
     }
-    
+
     public void addHeli(Helicopter h){
         helicopters.add(h);
     }
+
+    public String getcoordHQ(){
+        String pos;
+        pos = Integer.toString(HQcoordx)+' '+Integer.toString(HQcoordy);
+        return pos;
+    }
+
 }
