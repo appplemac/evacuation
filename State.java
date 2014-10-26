8448f6ef19bd4e74714769b18cbf842f37e2dbb4
Drixide77 <adria.abella.93@gmail.com>
2014-10-26 20:00:34 +0100

    Improved Random generation

--- State.java
+++ State.java
@@ -26,12 +26,13 @@ public class State {
         numHeli = numHeliPerHQ;
     }
 
-    public State(State orig){
-        hqs = (ArrayList)orig.hqs.clone();
-        groups = (ArrayList)orig.groups.clone();
-        numHeli = orig.getnumHeli();
-        numits = orig.numits;
-        idHeliMaximo = orig.idHeliMaximo;
+    public State(State state) {
+        hqs = new ArrayList<Headquarter>();
+        groups = new ArrayList<Group>();
+        for (Headquarter hq: state.hqs) hqs.add(new Headquarter(hq));
+        for (Group g: state.groups) groups.add(new Group(g));
+        numits = 0;
+        idHeliMaximo = 0;
     }
 
     public void addHQ(Headquarter hq){
@@ -54,10 +55,20 @@ public class State {
         return count;
     }
 
+    public Helicopter getHeliWithLongestTravelTime() {
+        Helicopter res = hqs.get(0).getHelicopter(0);
+        int max = 0;
+        for (Headquarter hq:hqs) {
+            Helicopter heli = hq.getHeliWithLongestTravelTime(this);
+            if (heli.getTravelTime(this) > max) res = heli;
+        }
+        return res;
+    }
+
     public int getnumHeli(){
         return numHeli;
     }
-
+    
     public void moveGroup(int idHeli1, int idHeli2, int indexOrigen, int indexDestino){
         Pair<Integer,Integer> aux;
         int indexHQ = idHeli1 / numHeli;
@@ -65,51 +76,26 @@ public class State {
         Headquarter auxHQ = hqs.get(indexHQ);
         Helicopter auxHeli = auxHQ.getHelicopter(indexHeli);
         aux = auxHeli.getGroup(indexOrigen);
+        ArrayList< Pair<Integer,Integer> > itinerary1 = auxHeli.getItinerary();
+        if (itinerary1.get(indexOrigen).getSecond() == 0) {
+            if (indexOrigen+1 < itinerary1.size()) {
+                if (itinerary1.get(indexOrigen+1).getSecond() == 1) {
+                    itinerary1.get(indexOrigen+1).setSecond(0);
+                }
+            }
+        }
         auxHeli.getItinerary().remove(indexOrigen);
         indexHQ = idHeli2 / numHeli;
         indexHeli = idHeli2 % numHeli;
         auxHQ = hqs.get(indexHQ);
         Helicopter auxHeli2 = auxHQ.getHelicopter(indexHeli);
         auxHeli2.getItinerary().add(indexDestino, aux);
-        
-        ArrayList< Pair<Integer,Integer> > itinerary1 = auxHeli.getItinerary();
         ArrayList< Pair<Integer,Integer> > itinerary2 = auxHeli2.getItinerary();
-        int sumcargo = 0;
-        boolean first = true;
-        for(Pair<Integer,Integer> auxp1 : itinerary1){
-            Group g1 = groups.get(auxp1.getFirst());
-            int people = g1.getNumPeople();
-            if (first){
-                auxp1.setSecond(0);
-                sumcargo = sumcargo+people;
-                first = false;
-            }
-            else if (people + sumcargo <= 15 && !first){
-                auxp1.setSecond(1);
-                sumcargo = sumcargo + people;
-            }
-            else {
-                auxp1.setSecond(0);
-                sumcargo = people;
-            }
-        }
-        sumcargo = 0;
-        first = true;
-        for(Pair<Integer,Integer> auxp2 : itinerary2){
-            Group g2 = groups.get(auxp2.getFirst());
-            int people = g2.getNumPeople();
-            if (first){
-                auxp2.setSecond(0);
-                sumcargo = sumcargo+people;
-                first = false;
-            }
-            else if (people + sumcargo <= 15 && !first){
-                auxp2.setSecond(1);
-                sumcargo = sumcargo + people;
-            }
-            else {
-                auxp2.setSecond(0);
-                sumcargo = people;
+        if (itinerary2.get(indexDestino).getSecond() == 1) {
+            if (indexDestino+1 < itinerary2.size() && indexDestino-1 > 0) {
+                if (itinerary2.get(indexDestino+1).getSecond() == 1 && itinerary2.get(indexDestino-1).getSecond() == 1) {
+                    itinerary2.get(indexDestino).setSecond(0);
+                }
             }
         }
     }
@@ -121,56 +107,38 @@ public class State {
         auxHQ.getHelicopter(indexHeli).joinGroups(index1, index2);
     }
 
-    public List<Successor> generateSuccessors (){
-        List<Successor> successors = new ArrayList<Successor>();
-        State orig = this;
-        for (Headquarter hq : hqs){
-            for (Helicopter h : hq.getHelicopters()){
-                for(Headquarter hq2 : hqs){
-                    for(Helicopter h2 : hq2.getHelicopters()){
-                        if (h.getIdent() != h2.getIdent() && h.getItineraryLength()>1){
-                            State modif = new State(orig);
-                            Random rand = new Random();
-                            int randNum = rand.nextInt();
-                            randNum = Math.abs(randNum);
-                            int index1 = randNum % (h.getItineraryLength());
-                            randNum = rand.nextInt();
-                            randNum = Math.abs(randNum);
-                            int index2 = randNum % (h2.getItineraryLength());
-                            modif.moveGroup(h.getIdent(), h2.getIdent(), index1, index2);
-                            //System.out.println("Moving things around");
-                            String explanation;
-                            explanation = "move group " + index1 + " of helicopter " + h.getIdent() + " to " + index2 + " of " + h2.getIdent();
-                            successors.add(new Successor(explanation, modif));
-                        } else {
-                            State modif = new State(orig);
-                            Boolean found = false;
-                            String explanation = "Nothing joined";
-                            for (int i = 0; i < h.getItinerary().size(); ++i) {
-                                for (int j = i+1; j < h.getItinerary().size(); ++j) {
-                                    if ((i < h.getItineraryLength() - 2) && (j < h.getItineraryLength() - 1))  {
-                                        if (h.getItinerary().get(i).getSecond() == 0 && h.getItinerary().get(i+1).getSecond() == 0 &&
-                                        h.getItinerary().get(j).getSecond() == 0 && h.getItinerary().get(j+1).getSecond() == 0 &&
-                                        groups.get(h.getItinerary().get(i).getFirst()).getNumPeople() +
-                                        groups.get(h.getItinerary().get(j).getFirst()).getNumPeople() <= h.getCapacity()) {
-                                            modif.joinRescues(h.getIdent(),i,j);
-                                            //System.out.println("Joining things around");
-                                            explanation = "Joined groups " + i + " and " + j + " of helicopter " + h.getIdent();
-                                            found = true;
-                                            break;
-                                        }
-                                    }
-                                }
-                                if (found) break;
-                            }
-                            if (found) successors.add(new Successor(explanation, modif));
-                        }
-                    }
-                }
+    public int getRandom(int max) {
+        int value = 0;
+        Random rand = new Random();
+        Random rand2 = new Random();
+        value = (rand.nextInt(max) * rand.nextInt(max)) % max;
+        return value;
+    }
+    
+    public ArrayList<Successor> generateSuccessors (){
+        ArrayList<Successor> successors = new ArrayList<Successor>();
+        String explanation;
+        for (int i = 0; i < 1000; ++i) {
+            Headquarter hqOfHeli1 = hqs.get(getRandom(hqs.size()));
+            Helicopter heli1 = hqOfHeli1.getHelicopter(getRandom(hqOfHeli1.getNumHelicopters()));
+            State modified = new State(this);
+            Headquarter hqOfHeli2 = hqs.get(getRandom(hqs.size()));
+            Helicopter heli2 = hqOfHeli2.getHelicopter(getRandom(hqOfHeli2.getNumHelicopters()));
+            while (heli2 == heli1) heli2 = hqOfHeli2.getHelicopter(getRandom(hqOfHeli2.getNumHelicopters()));
+            int indexGrp1 = getRandom(heli1.getItineraryLength());
+            int indexGrp2 = getRandom(heli2.getItineraryLength());
+            modified.moveGroup(heli1.getIdent(), heli2.getIdent(), indexGrp1, indexGrp2);
+            explanation = "Moved group " + indexGrp1 + " from heli " + heli1.getIdent() + " to position " + indexGrp2 + " from heli " + heli2.getIdent();
+            indexGrp1 = getRandom(heli2.getItineraryLength());
+            indexGrp2 = getRandom(heli2.getItineraryLength());
+            while (indexGrp1 == indexGrp2) indexGrp2 = getRandom(heli2.getItineraryLength());
+            if (heli2.getGroup(indexGrp1).getSecond() == 0
+                    && heli2.getGroup(indexGrp2).getSecond() == 0) {
+                modified.joinRescues(heli2.getIdent(), indexGrp1, indexGrp2);
             }
+            explanation = explanation + " AND joined group " + indexGrp1 + " from heli " + heli1.getIdent() + " with group " + indexGrp2 + " from heli " + heli2.getIdent();
+            successors.add(new Successor(explanation, modified));
         }
-        ++numits;
-
         return successors;
     }
 
@@ -184,36 +152,34 @@ public class State {
             return false;
         }
     }
-              /*
-    public int calculateHeuristic(){
-        int itineraryLengthSum = 0;
-        int maxItineraryLength = 0;
-        int numHelis = 0;
-        for (Headquarter hq : hqs){
-            for (Helicopter h: hq.getHelicopters()){
-                int length = h.getItineraryLength();
-                if (length > maxItineraryLength) maxItineraryLength = length;
-                itineraryLengthSum += length;
-                ++numHelis;
-            }
-        }
 
-        int meanItineraryLength = itineraryLengthSum / numHelis;
-        int heuristic = meanItineraryLength + maxItineraryLength^2;
-        return heuristic;
-    }       */
+//    public int calculateHeuristic(){
+//        int itineraryLengthSum = 0;
+//        int maxItineraryLength = 0;
+//        int numHelis = 0;
+//        for (Headquarter hq : hqs){
+//            for (Helicopter h: hq.getHelicopters()){
+//                int length = h.getItineraryLength();
+//                if (length > maxItineraryLength) maxItineraryLength = length;
+//                itineraryLengthSum += length;
+//                ++numHelis;
+//            }
+//        }
+//
+//        int meanItineraryLength = itineraryLengthSum / numHelis;
+//        int heuristic = meanItineraryLength + maxItineraryLength^2;
+//        return heuristic;
+//    }
 
     public int calculateHeuristic(){
-        ArrayList<Integer> times = new ArrayList<Integer>();
-        int max = 0;
+        int sum = 0;
         for (Headquarter hq : hqs){
-            for (Helicopter h: hq.getHelicopters()){
+            for (Helicopter h: hq.getHelicopters()) {
                 int time = h.getTravelTime(this);
-                //System.out.println(" "+time);
-                times.add(time);
-                max = max + time;
+                sum += time;
             }
         }
-        return max;
+        System.out.println(sum);
+        return sum;
     }
 }
